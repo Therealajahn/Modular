012427f642328327cb19e4484c8d23894fc20928
A'Jahn Kimble <therealajahn@gmail.com>
2023-05-05 15:43:04 -0500

    looping and one-shotting sample

--- sampler.js
+++ sampler.js
@@ -4,15 +4,53 @@ document.getElementById("start")?.addEventListener("click", async () => {
 });
 
 const player = new Tone.Player("knarl_loop.wav").toDestination();
+const start = 0.2;
+const end = 0.5;
+const stop = end - start;
 
 const slowerButton = document.querySelector("#slower");
 const fasterButton = document.querySelector("#faster");
+const controls = document.getElementById("controls");
+
+let loop = "one-shot";
+controls.addEventListener("click", (event) => {
+  if (event.target.type !== "radio") {
+    return;
+  }
+  const radio = event.target;
+  if (radio.classList[0] === "loop") {
+    console.log(radio.value);
+    loop = radio.value;
+  }
+});
 
 slowerButton.addEventListener("click", () => {
-  player.playbackRate = 0.5;
-  player.start();
+  playSample(0.5, loop);
 });
+
 fasterButton.addEventListener("click", () => {
-  player.playbackRate = 2;
-  player.start();
+  playSample(2, loop);
 });
+
+function playSample(rate, loop) {
+  player.playbackRate = rate;
+  switch (loop) {
+    case "loop":
+      loopSample();
+      break;
+
+    case "one-shot":
+      oneShot();
+      break;
+  }
+}
+function oneShot() {
+  player.start(start);
+  player.stop(`+${end}`);
+}
+
+function loopSample() {
+  player.start();
+  player.setLoopPoints(start, end);
+  player.loop = true;
+}
